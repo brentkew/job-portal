034f7075a50a75c7f47fc3e97af15c1e10ea8b22
Khalil Ahmad <brentkew@gmail.com>
2024-09-03 19:39:02 +0500

    Featured job section completed with application setpup

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $50,000 - $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300" href="">Tag 1</a>
+            <a class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300" href="">Tag 1</a>
+            <a class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300" href="">Tag 1</a>
+        </div>
+        <img class="rounded-xl" src="https://place-hold.it/42x42" alt="Company Logo" />
+    </div>
+
+</div>
---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,6 @@
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-3 h-3 bg-white inline-block"></span>
+    <h3 class="font-bold text-xl">{{ $slot }}</h3>
+</div>
+
+
